d0dd4f01146fbf69a119b683d990939cceb90f17
vsdrs12345-droid <vsdrs12345@gmail.com>
2026-08-02 22:48:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "attendify-v2";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+});
+
+
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+
+                cacheNames.map(function (cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(function (response) {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
